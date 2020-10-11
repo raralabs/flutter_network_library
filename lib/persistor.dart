e91433e1b0417f3fc3852455785ed20f041a075b
neonrender <neonrender@gmail.com>
2020-10-11 12:22:58 +0545

    minor fixes

--- lib/persistor.dart
+++ lib/persistor.dart
@@ -88,7 +88,7 @@ class Persistor{
 
   static Future<void> initialize({String databaseName = 'store3.db'}) async {
     
-    var dir = await getApplicationDocumentsDirectory();
+    var dir = await getTemporaryDirectory();
     Hive.init(dir.path);
     
     if(!kIsWeb)
