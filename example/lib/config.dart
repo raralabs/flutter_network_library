ac3384ce1e9c1d8572c43323d31c4cefd82f9f0c
Netesh5 <netesh.paudel@gmail.com>
2024-02-23 10:29:36 +0545

    fix: config files

--- example/lib/config.dart
+++ example/lib/config.dart
@@ -3,22 +3,11 @@ import 'package:flutter_network_library/domain.dart';
 
 var config = NetworkConfig(
   scheme: NetworkScheme.https,
-  host:  'api2.binance.com',
+  host: 'www.google.com',
 );
 
 var domains = {
-  'appState': Domain(
-    type: DomainType.basic,
-
-    path: {
-      'theme':(_)=>'/change-theme'
-    }
-  ),
-
-  'api':Domain(
-    cacheForSeconds: 5,
-    path: {
-      'list':(_)=>'/market'
-    }
-  )
+  'appState':
+      Domain(type: DomainType.basic, path: {'theme': (_) => '/change-theme'}),
+  'api': Domain(cacheForSeconds: 5, path: {'list': (_) => '/'})
 };
